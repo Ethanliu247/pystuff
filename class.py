6ab2d391c309d53f657f92b8e9be34a965c5abea
ShadowElf37 <ykey-cohen@emerywiener.org>
2016-11-05 10:54:21 -0500

    updated files

--- class.py
+++ class.py
@@ -1,5 +1,5 @@
 from math import floor
-from random import shuffle
+from random import choice, shuffle
 def team(classList):
 	shuffle(classList)
-	print("Team 1:\n", (classList[i] + "\n" for i in range(0, floor(len(classList)/2)), "Team 2:\n", (classList[i] + "\n" for i in range(1 + floor(len(classList)/2)), len(classList)), "The", ("left" if (shuffle([0, 1])[0] == 0) else "right"), "team goes first.")
+	print("Team 1:\n", (classList[i] + "\n" for i in range(0, floor(len(classList)/2))), "Team 2:\n", ((classList[i] + "\n" for i in range(1 + floor(len(classList)/2))), len(classList)), "The", ("left" if randint(0,1) == 0 else "right"), "team goes first.")
